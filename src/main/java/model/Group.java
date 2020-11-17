ccb567d00baf765995180765a0a30d7945e4ac08
Julian <j.hubatsch@boewa.de>
2020-11-17 10:00:40 +0100

    Klassen vervollständigt

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -1,6 +1,19 @@
 package model;
 
-
 public class Group {
-    
+
+    private String groupname;
+
+    public Group (String groupname){
+
+        this.groupname = groupname;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,29 @@
 package model;
 
 public class User {
+    private Group group;
+    private String username;
+
+    public User(String username, Group group){
+        this.username = username ;
+        this.group = group;
+    }
+
+    public Group getGroup(){
+        return this.group;
+    }
+
+    public void setGroup (Group group){
+        this.group = group;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
+
+
