be73759b8e7a395f71c33d0293a384f9fe04acf8
Jannik Gerdes <jannik.gerdes@hmmh.de>
2020-11-17 09:27:00 +0100

    Issue #4 - Removed autogenerated catch blocks. Updated .gitignore

--- src/main/java/controller/AllTreatmentController.java
+++ src/main/java/controller/AllTreatmentController.java
@@ -193,7 +193,6 @@ public class AllTreatmentController {
             stage.setResizable(false);
             stage.showAndWait();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -213,7 +212,6 @@ public class AllTreatmentController {
             stage.setResizable(false);
             stage.showAndWait();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
